348820a755cc78d3e3034b952b29326f2108162d
Takatoshi Matsumoto <toqoz403@gmail.com>
2013-02-12 11:16:16 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Circler::Application.config.secret_token = 'ebe2349995fceb7c8e14ccc6fd3b6b564d53b31f2bdf267b51aee70002fdb4a3820ee141a17c1782509156f7b921f5bd584dd75f1652ef85bdb12c99d9d02e4c'
